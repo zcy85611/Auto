4f57b0643b7b74d6f011f2009bc00f5b387efe60
skygunner5 <skygunner5@hotmail.com>
2025-02-16 11:35:42 +0800

    updated

--- package/luci-app-xray/htdocs/luci-static/resources/view/xray.js
+++ package/luci-app-xray/htdocs/luci-static/resources/view/xray.js
@@ -5,46 +5,38 @@
 'require rpc';
 'require form';
 'require tools.widgets as widgets';
-
 var callServiceList = rpc.declare({
 	object: 'service',
 	method: 'list',
-	params: [ 'name' ],
-	expect: { 'xray': {} }
+	params: ['name'],
+	expect: {
+		'xray': {}
+	}
 });
-
 return view.extend({
 	load: function() {
-		return Promise.all([
-			L.resolveDefault(callServiceList('xray')),
-			uci.load('xray')
-		]);
+		return Promise.all([L.resolveDefault(callServiceList('xray')), uci.load('xray')]);
 	},
 	render: function(res) {
 		var running = Object.keys(res[0].instances || {}).length > 0;
 		var button, status;
 		if (running) {
-		    status = '<font color="green" size=3><b>' + _('Service is running') + '</b></font>';}
-		    else {
-		    status = '<font color="red" size=3><b>' + _('Service is not running') + '</b></font>';}
-		    
+			status = '<font color="green" size=3><b>' + _('Service is running') + '</b></font>';
+		} else {
+			status = '<font color="red" size=3><b>' + _('Service is not running') + '</b></font>';
+		}
 		var m, s, o;
-		
 		m = new form.Map('xray');
 		m.title = _('Dedicated Line');
-		
 		s = m.section(form.TypedSection);
 		s.title = _('Running Status');
 		s.description = status;
 		s.anonymous = true;
-
 		s = m.section(form.TypedSection, 'basic', _('Basic Settings'));
 		s.addremove = false;
 		s.anonymous = true;
-		
 		o = s.option(form.Flag, 'enabled', _('Enable'));
 		o.rmempty = false;
-
 		o = s.option(form.ListValue, 'loglevel', _("Log Level"));
 		o.value("none");
 		o.value("error");
@@ -53,17 +45,15 @@ return view.extend({
 		o.value("debug");
 		o.default = "none";
 		o.rmempty = false;
-
+		o = s.option(form.Value, 'serverIP', _('Server IP'));
+		o.rmempty = false;
 		o = s.option(form.Value, 'serverAddr', _('Server Address'));
 		o.rmempty = false;
-
 		o = s.option(form.Value, 'port', _('Port'));
 		o.datatype = 'and(port,min(1))'
 		o.rmempty = false;
-
 		o = s.option(form.Value, 'uuid', _('UUID'));
 		o.rmempty = false;
-
 		return m.render();
 	}
 });
